25e102778391e4f577f7b2d7ada0822383a5cd06
arturocutanda <32671607+arturocutanda@users.noreply.github.com>
2021-05-04 13:05:50 +0200

    creación de la funcion de la primera opcion

--- juego_adivina_numeros/juego.sh
+++ juego_adivina_numeros/juego.sh
@@ -5,7 +5,43 @@
 # EL JUGADOR DISPONDRA DE 5 OPCIONES PARA PODER ACERTAR EL NUMERO, EN CADA UNA DE ESTAS OPCIONES SE DIRA SI Y EL NUMERO ES MAYOR O MENOR
 # AL FINAL DE CADA UNA DE LAS JUGADAS SE MOSTRARA EL NUERMO DE VECES JUGADAS, EL NUMERO DE VECES QUE SE HA PERDIDO Y EL NUERMO DE VECES QUE SE HA GANADO
 
-##
+#### CREACIÓN DE LA FUNCION DE LA PRIMERA OPCION (Se ha de revisar el control de los intentos, no funiona correctamente)
+
+fprimera() {
+	nmaximo=25 #Numero mas alto posible
+	intentos=0 #Numero de intentos para acertar el numero
+
+
+	echo "Se ha iniciado el modo facil, el numero ha acertar esta entre el 0 y al 25"
+
+	nacertar=$((RANDOM%25)) #Numero que el usuario debe acertar
+	echo $nacertar
+	echo "¿Cual crees el numero que estoy pensado?"
+
+	while [[ $intentos -lt 5 ]]
+	do
+		read nusuario #Numero que propone el usuario
+
+		if [[ $nusuario -lt $nacertar ]]; then
+			echo "El numero que estoy pensado en mayor que $nusuario"
+			intentos=$(($intentos+1))
+		elif [[ $nusuario -gt $nmaximo ]];then
+			echo "Recuerda que el rango es de 0 al 25, está no te la cuento"
+		elif [[ $nusuario -gt $nacertar ]];then
+			echo "El numero que estoy pensado en menor que $nusuario"
+			intentos=$(($intentos+1))
+		elif [[ $nusuario -eq $nacertar ]]; then
+			echo "ENHORABUENA!! Has acertado el numero que estoy pensado"
+			intentos=5
+		fi
+
+	done
+	
+		if [[ $intentos -eq 5 ]]; then 
+			echo "Se han terminado los intentos"
+
+		fi
+}
 
 
 ##### 	DECLARACION DE VARIABLES DEL MENU ######
@@ -23,11 +59,11 @@ echo "#####################################"
 echo "#                                   #"
 echo "#    SELECCIONE EL MODO DE JUEGO    #"
 echo "#                                   #"
-echo "#      1)  $primera                #"
-echo "#      2)  $segunda                    #"
-echo "#      3)  $tercera                    #"
-echo "#      4)  $cuarta                  #"
-echo "#      5)  $quinta                  #"
+echo "#      1)  ${primera^^}                #"
+echo "#      2)  ${segunda^^}                    #"
+echo "#      3)  ${tercera^^}                    #"
+echo "#      4)  ${cuarta^^}                  #"
+echo "#      5)  ${quinta^^}                  #"
 echo "#                                   #"
 echo "#####################################"
 
@@ -38,12 +74,12 @@ while [[ $x -eq 0 ]]
 	do
 		read OpcionUsuario
 
-		case $OpcionUsuario in
+		case ${OpcionUsuario,,} in
 			$primera)
-				echo "Se ha escofigo la primera opción"
+				fprimera
 				x=1;;
 			$segunda)
-				echo "Se ha escogido la segunda opción"
+				fprimera
 				x=1;;
 			$tercera)
 				echo "Se ha escogido la tercera opcion"
@@ -61,29 +97,4 @@ while [[ $x -eq 0 ]]
 	done
 
 
-<< COMENTADO     ##  NO CREO QUE SEA LA MEJOR OPCIÓN ##
-x=0
-
-while [[ $x == 0 ]]
-	do
-    		read OpcionUsuario
-    		if [[ (( $OpcionUsuario != $primera )) ]]; then
-        		x=0 
-        		echo "Se debe introducir una opción adecuada y en minusculas"
-    		elif [[ (($OpcionUsuario != "facil")) ]]; then
-        		x=0 
-        		echo "Se debe introducir una opción adecuada y en minusculas"
-    		elif [[ (($OpcionUsuario != "medio")) ]];then 
-        		x=0 
-        		echo "Se debe introducir una opción adecuada y en minusculas"
-    		elif  [[ (($OpcionUsuario != "elevado")) ]];then
-       			x=0 
-        		echo "Se debe introducir una opción adecuada y en minusculas"
-	    	elif  [[ (($OpcionUsuario != "dificil")) ]] ; then
-        		x=0 
-        		echo "Se debe introducir una opción adecuada y en minusculas"
-	    	fi
-	done
-
-COMENTADO
 
--- juego_adivina_numeros/pruebas.sh
+++ juego_adivina_numeros/pruebas.sh
@@ -1,1 +1,10 @@
-prueba prueba prueba
+#!/bin/bash
+
+x=0
+
+
+while [[ $x -lt 5  ]]
+do	
+	echo $x
+	x=$(($x+1))
+done
